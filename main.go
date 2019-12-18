e249a144a02253565d22b294be9052f41b06ba51
ranqiwu <857271370@qq.com>
2019-12-18 13:19:17 +0800

    Update Access-Control-Allow-Headers

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ var dirHandle = http.FileServer(http.Dir("."))
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
